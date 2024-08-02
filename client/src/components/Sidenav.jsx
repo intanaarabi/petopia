912000ef2da6acbe3baadc95224dfad517485b1a
Dayang Intan <intan.abangarabi@gmail.com>
2024-08-03 00:26:54 +0800

    feat: add logout

--- client/src/components/Sidenav.jsx
+++ client/src/components/Sidenav.jsx
@@ -1,16 +1,26 @@
-import { Outlet, NavLink,useLocation  } from 'react-router-dom';
+import { Outlet, NavLink,useLocation, useNavigate  } from 'react-router-dom';
 import links from '../config/links';
 import { getPageTitle } from '../utils/pageTitle';
 import { FaUser } from "react-icons/fa";
+import { IoLogOut } from "react-icons/io5";
+import { useDispatch } from 'react-redux';
+import { logout } from '../redux/features/auth/authSlice';
 
 const Sidenav = () => {
 
     const location = useLocation();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const logoutUser = () => {
+      dispatch(logout())
+      navigate('/login')
+    }
 
     return (
         <>
         <div className="h-full flex flex-row">
-            <div className="min-h-screen bg-white w-60">
+            <div className="min-h-screen bg-white w-60 flex flex-col">
                     <div className="text-typography-primary font-bold text-2xl pl-10 py-6 border-b border-background-primary mb-8">
                         PETOPIA
                     </div>
@@ -32,6 +42,12 @@ const Sidenav = () => {
                         ))
                        }
                     </div>
+                    <div className='flex-grow'></div>
+                    <button onClick={logoutUser} className="text-typography-secondary font-light mb-8 group flex flex-row items-center pl-10 gap-2">
+                      <IoLogOut className={` group-hover:text-button-accent transition-all duration-200 text-xl`} />
+                      <span className={`group-hover:text-typography-primary group-hover:font-bold  transition-all duration-200 text-sm `}>Log Out</span>
+                    </button>
+
             </div>
             <div className="flex-grow flex flex-col gap-6 p-8">
             <div className='flex flex-row items-center'>
