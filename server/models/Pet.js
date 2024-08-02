eea1df6a9dd14192c58a333a013e3a57c53f3814
Dayang Intan <intan.abangarabi@gmail.com>
2024-08-02 15:37:04 +0800

    wip:  backend models (User and Pets)

---
+++ server/models/Pet.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const petSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  sex: {
+    type: String,
+    required: true,
+    enum: ['male','female']
+  },
+  species: {
+    type: String,
+    required: true,
+    enum: ['cat','dog'],
+  },
+  breed: {
+    type: String,
+    required: false,
+  },
+  dob: {
+    type: Date,
+    required: false
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
+
+const Pet = mongoose.model('Pet', petSchema);
+module.exports = Pet;
