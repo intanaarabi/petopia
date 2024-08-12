a97cf44081c193515f01314d5bc7b88f045f8670
Dayang Intan <intan.abangarabi@gmail.com>
2024-08-12 17:31:29 +0800

    wip: add react hook form to events popup page, add skeleton ui for upcoming events

--- client/src/components/Cards/PetCardCalendar.jsx
+++ client/src/components/Cards/PetCardCalendar.jsx
@@ -1,10 +1,81 @@
 import Calendar from 'react-calendar'
+import { MdOutlinePets } from 'react-icons/md';
+import AddButton from '../Buttons/AddButton';
+import { useState } from 'react';
+import AddEventsPopup from '../Popup/AddEventsPopup';
+
+const PetEventMarker = () => {
+    return <MdOutlinePets className='m-auto text-button-accent text-xs'/>
+}
 
 const PetCardCalendar = () => {
+    const [isPopupOpen,setIsPopupOpen] = useState(false)
+    const openPopup = () => setIsPopupOpen(true)
+    const closePopup = () => setIsPopupOpen(false)
+
+    
+    const markedDates = [
+        new Date(2024, 7, 14), 
+        new Date(2024, 7, 15),  
+        new Date(2024, 7, 20), 
+      ];
+    
+    const isDateMarked = (date) => {
+    return markedDates.some(markedDate =>
+        markedDate.getFullYear() === date.getFullYear() &&
+        markedDate.getMonth() === date.getMonth() &&
+        markedDate.getDate() === date.getDate()
+    );
+    };
+
+      // Get the current date
+    const currentDate = new Date();
+
+    // Extract day, month, and year
+    const day = String(currentDate.getDate()).padStart(2, '0'); // Pad with leading zeros if necessary
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Months are zero-based, so add 1
+    const year = currentDate.getFullYear();
+
+    // Format the date as dd/mm/yyyy
+    const formattedDate = `${day}/${month}/${year}`;
+
     return (
-        <div className='card p-6'>
-            <Calendar/>
+        <>
+           <div className='flex gap-4 flex-col'>
+            <div className='card p-6'>
+                <Calendar
+                    tileContent={({ date, view }) =>
+                    view === 'month' && isDateMarked(date) ? (
+                        <PetEventMarker/>
+                    ) : null
+                    }
+                    />
+            </div>
+            <div className='card p-6 flex flex-col gap-4 '>
+                <div className='flex flex-col'>
+                        <p className="font-bold text-md flex flex-row">
+                            Today's Events
+                            <div className='flex-grow'></div>
+                            <AddButton mini={true} onClick={openPopup}/>
+                
+                        </p>
+                        <span><p className="label-secondary">{formattedDate}</p></span>
+                </div>
+                <div className='relative border-[1px] border-background-primary rounded-xl shadow-2xl shadow-gray-400/50 p-4 flex flex-row gap-4 overflow-hidden'>
+                    <div className='absolute h-full w-[10px] left-0 top-0 bg-accent-primary '></div>
+                    <div className='flex flex-col pl-4'>
+                        <p className='label-secondary'>1.00 PM - 2.00 PM</p>
+                        <p className='font-bold text-typography-primary'>Groomer's</p>
+                        <p className='label-secondary font-italic'>Zul Erwan Grooming</p>
+                    </div>
+                </div>
+            </div>
         </div>
+        <AddEventsPopup isOpen={isPopupOpen}  onClose={closePopup}/>
+        </>
+     
+
+        
     )
 }
 
---
+++ client/src/components/Popup/AddEventsPopup.jsx
@@ -0,0 +1,183 @@
+
+import { useForm } from "react-hook-form"
+import Popup from "./Popup"
+import { useDispatch, useSelector } from "react-redux"
+import { selectCurrentPetMetadata } from "../../redux/features/pets/currentPetSlice"
+import ClipLoader from "react-spinners/ClipLoader"
+import { selectLogsLoading } from "../../redux/features/petLogs/logsSlice"
+import { useEffect, useState } from "react"
+
+const AddEventsPopup = ({isOpen, onClose}) => {
+    const {register, handleSubmit, setValue, formState: {errors}, reset } = useForm()
+    const dispatch = useDispatch()
+    const pet = useSelector(selectCurrentPetMetadata)
+    const loading = useSelector(selectLogsLoading)
+    const [isAllDay, setIsAllDay] = useState(false);
+
+    useEffect(() => {
+        if (!isOpen) {
+            reset();
+            setIsAllDay(false)
+        }
+    }, [isOpen, reset]);
+
+    if (!isOpen) return null
+
+    const onSubmit = (data) => {
+      console.log(data)
+    }
+
+
+    const handleToggleAllDay = () => {
+        setIsAllDay(!isAllDay);
+    
+        if (!isAllDay) {
+          // Set default start and end times when "All day" is toggled on
+          setValue('startTime', '00:00', { shouldValidate: true });
+          setValue('endTime', '23:59', { shouldValidate: true });
+        } else {
+          // Clear the times when "All day" is toggled off
+          setValue('startTime', '');
+          setValue('endTime', '');
+        }
+      };
+
+    return(
+        <Popup onClose={onClose}>
+            <div className="flex flex-col gap-4">
+
+            <p className="header">Add New Event</p>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="flex flex-col flex-grow gap-3">
+                                <div className="flex flex-col gap-1">
+                                    <label 
+                                        htmlFor="title" 
+                                        className="label-secondary">Title</label>
+                                    <input 
+                                        id="title"
+                                        type="text" 
+                                        {...register('title', {required: 'Title is required'})}
+                                        className={`w-full input ${errors.name ? 'border-red-500' : '' }`}
+                                    />
+                                    {errors.title && <span className='input-error'>{errors.title.message}</span>}
+                                </div>
+                                <div className="flex flex-col gap-1">
+                                    <label 
+                                        htmlFor="location" 
+                                        className="label-secondary">Location</label>
+                                    <input 
+                                        id="location"
+                                        type="text" 
+                                        {...register('location', {required: 'Location is required'})}
+                                        className={`w-full input ${errors.name ? 'border-red-500' : '' }`}
+                                    />
+                                    {errors.location && <span className='input-error'>{errors.location.message}</span>}
+                                </div>
+
+                                <div className="flex flex-row gap-4 w-full">
+                                        <label htmlFor="allDay" className="label-secondary">
+                                        All day
+                                        </label>
+                                        <input
+                                        type="checkbox"
+                                        id="allDay"
+                                        checked={isAllDay}
+                                        onChange={handleToggleAllDay}
+                                        />
+                                    </div>
+
+                                <div className="flex flex-row gap-1 w-full">
+                                    <div className="flex flex-col gap-1 w-full">
+                                            <label 
+                                                htmlFor="startDate" 
+                                                className="label-secondary">Start Date</label>
+                                            <input 
+                                                id="startDate"
+                                                type="date" 
+                                                {...register('startDate', {required: 'Start date is required'})}
+                                                className={`w-full input ${errors.dob ? 'border-red-500' : '' }`}
+                                            />
+                                            {errors.startDate && <span className='input-error'>{errors.startDate.message}</span>}
+                                    </div>
+                                    <div className="flex flex-col gap-1 w-full">
+                                            <label 
+                                                htmlFor="startTime" 
+                                                className="label-secondary">Start Time</label>
+                                           <input
+                                                id="startTime"
+                                                type="time"
+                                                {...register('startTime', { required: !isAllDay })}
+                                                disabled={isAllDay}
+                                                className={`disabled:text-typography-secondary w-full input ${errors.startTime ? 'border-red-500' : ''}`}
+                                                />
+                                    </div>
+                                </div>
+                             
+                                <div className="flex flex-row gap-1 w-full">
+                                                                       
+                                    <div className="flex flex-col gap-1 w-full">
+                                            <label 
+                                                htmlFor="endDate" 
+                                                className="label-secondary">End Date</label>
+                                            <input 
+                                                id="endDate"
+                                                type="date" 
+                                                {...register('endDate', {required: 'End date is required'})}
+                                                className={`w-full input ${errors.dob ? 'border-red-500' : '' }`}
+                                            />
+                                            {errors.endDate && <span className='input-error'>{errors.endDate.message}</span>}
+                                    </div>
+                                                                          
+                                    <div className="flex flex-col gap-1 w-full">
+                                            <label 
+                                            htmlFor="endTime" 
+                                            className="label-secondary">End Time</label>
+                                          <input
+                                                id="endTime"
+                                                type="time"
+                                                {...register('endTime', { required: !isAllDay })}
+                                                disabled={isAllDay}
+                                                className={`disabled:text-typography-secondary w-full input ${errors.endTime ? 'border-red-500' : ''}`}
+                                                />
+                                            {errors.endTime && <span className='input-error'>{errors.endTime.message}</span>}
+                                        </div>
+                                </div>
+                                 
+
+                                <div className="flex flex-col gap-1 w-full">
+                                    <label 
+                                        htmlFor="type" 
+                                        className="label-secondary">Event Type</label>
+                                    <select
+                                        id="type"
+                                        className="input w-full"
+                                        defaultValue="veterinary"
+                                        {...register('type')}
+                                    >
+                                        <option value="veterinary">Veterinary</option>
+                                        <option value="grooming">Grooming</option>
+                                    </select>
+                                </div>
+
+                                <button disabled={loading.add} type="submit" className="mt-4 flex flex-row items-center justify-center gap-4 bg-typography-primary rounded-lg px-4 py-2 font-bold text-white hover:opacity-80 transition-all duration-300 ">
+                                    {loading.add && (
+                                    <>
+                                            <ClipLoader
+                                                color="#fff"
+                                                size={15}
+                                                aria-label="Loading Spinner"
+                                                data-testid="loader"
+                                            /> <p>Creating ...</p>
+                                    </> 
+                                    )} 
+                                    {!loading.add && (<p>Create Event</p>)}
+                                </button>
+                    </div>
+                </form>
+            </div>
+            
+        </Popup>
+    )
+}
+
+export default AddEventsPopup
